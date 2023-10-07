1a5735ec1d1e425a9c853396f40bae3cb008e2aa
Pankaj Gupta <130819429+pankaj301@users.noreply.github.com>
2023-10-07 16:13:49 +0530

    Update TextJenkins.java
    updated2

--- TextJenkins.java
+++ TextJenkins.java
@@ -1,6 +1,7 @@
 public class TextJenkins {
     public static void main(String[] args) {
-        System.out.println("This is testing for jenkins....");
+        System.out.println("This is testing for jenkins1....");
+        System.out.println("This is testing for jenkins2....");
     }
     
 }
