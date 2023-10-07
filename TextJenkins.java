5facab932abd571d66535bf9e602886a2b7fc1c1
Pankaj Gupta <130819429+pankaj301@users.noreply.github.com>
2023-10-07 13:48:45 +0530

    InitialCommit
    This 1st commit

---
+++ TextJenkins.java
@@ -0,0 +1,7 @@
+public class TextJenkins {
+    public static void main(String[] args) {
+        System.out.println("This is testing for jenkins....");
+    }
+    
+}
+                                                                                                        
